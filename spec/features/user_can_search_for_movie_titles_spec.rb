0cd6f317c5e4cf608f6e2482cd5367098d43dd7b
Chantal Justamond <cjustamm66@me.com>
2018-04-14 18:14:15 -0600

    movie titles test passing again :baby_chick:

--- spec/features/user_can_search_for_movie_titles_spec.rb
+++ spec/features/user_can_search_for_movie_titles_spec.rb
@@ -10,9 +10,9 @@ describe 'user can search by movie titles' do
   VCR.use_cassette('feature/user_can_search_for_movies') do
     it 'should see movie titles' do
       visit root_path
+      fill_in 'Enter Movie Titles', with: 'Iron man'
 
-      fill_in 'Movie Titles', with: 'Iron man'
-      click_button 'Search'
+      click_on 'Search'
 
       expect(current_path).to eq(search_path)
       expect(page).to have_content('Iron Man')
