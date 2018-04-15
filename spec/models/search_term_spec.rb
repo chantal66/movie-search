3f7055d955b4f248db8d4daacf30a2375779940d
Chantal Justamond <cjustamm66@me.com>
2018-04-14 19:13:49 -0600

    model test added

--- spec/models/search_term_spec.rb
+++ spec/models/search_term_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SearchTerm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'is valid with a value' do
+      value = SearchTerm.new(value: 'Iron man')
+      expect(value).to be_valid
+    end
+
+    it 'is invalid without a value' do
+      value = SearchTerm.new(value: nil)
+      expect(value).to be_invalid
+    end
+  end
 end
